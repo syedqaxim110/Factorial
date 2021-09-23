6801d06cca46817655607f0056aaf4974de38d77
Syed Qasim <87519159+syedqaxim110@users.noreply.github.com>
2021-09-23 15:05:38 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            long fact = GetFactorial(number);
+
+            Console.WriteLine("The factorial of {0} is {1}", number, fact);
+
+            Console.ReadKey();
+
+        }
+
+
+
+        private static long GetFactorial(int number)
+        {
+
+            if (number == 0)
+            {
+
+                return 1;
+
+            }
+
+            return number * GetFactorial(number - 1);
+
+        }
+
+            
+    }
+}
